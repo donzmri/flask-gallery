506acf95cedf75e15440edb7231b8a429be6df65
donzmri <chuydoni@gmail.com>
2025-09-18 19:44:43 +0800

    Update app.py and requirements.txt

--- app.py
+++ app.py
@@ -3,16 +3,17 @@ import os
 
 app = Flask(__name__)
 
-# Folder untuk file gambar
-IMAGE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images')
+# Folder tempat menyimpan gambar di dalam "static/images"
+IMAGE_FOLDER = os.path.join(os.getcwd(), "static", "images")
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Flask Gallery API is running. Access your files via /images/<filename>"
+    return "Flask Gallery API is running. Access images via /images/<filename>"
 
-@app.route('/images/<filename>')
+# Route untuk mengakses gambar
+@app.route("/images/<path:filename>")
 def serve_image(filename):
     return send_from_directory(IMAGE_FOLDER, filename)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run()
